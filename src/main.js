c86f9eaaff3381f53b10e6ce798ff6a232d728f0
Angry Fennec <shellfix@gmail.com>
2021-04-10 10:10:23 +0300

    Подготавливает отрисовку моков

--- src/main.js
+++ src/main.js
@@ -8,24 +8,31 @@ import {createFilmsListTemplate} from './view/films-list';
 import {createFilmsListExtraTemplate} from './view/films-list-extra';
 import {createPopupTemplate} from './view/popup';
 import {createFooterStatisticsTemplate} from './view/footer-statistics';
+import {generateFilm} from './mock/film';
 
-const EXTRA_FILMS_QUANTITY = 2;
-const FILMS_QUANTITY = 5;
 const TOP_RATED_TITLE = 'Top rated';
 const MOST_COMMENTED_TITLE = 'Most commented';
 const TOP_RATED_MODIFIER = 'top-rated';
 const MOST_COMMENTED_MODIFIER = 'most-commented';
+const MOCK_FILMS_QUANTITY = 20;
+const EXTRA_MOCK_FILMS_QUANTITY = 2;
+const FILMS_PER_STEP = 5;
 
-const renderFilmCards = (section, quantity) => {
+
+const renderFilmCards = (section, quantity, mocks, start = 0) => {
   const containerElement = section.querySelector('.films-list__container');
-  for (let i = 0; i < quantity; i++) {
-    render(containerElement, createFilmCardTemplate(), 'beforeend');
+  for (let i = start; i < quantity; i++) {
+    render(containerElement, createFilmCardTemplate(mocks[i]), 'beforeend');
   }
 };
 
 const render = (container, template, place) => {
   container.insertAdjacentHTML(place, template);
 };
+
+// создание моковых массивов
+const mockFilms = new Array(MOCK_FILMS_QUANTITY).fill().map(generateFilm);
+const extraMockFilms = new Array(EXTRA_MOCK_FILMS_QUANTITY).fill().map(generateFilm);
 
 const siteHeaderElement = document.querySelector('.header');
 const siteMainElement = document.querySelector('.main');
@@ -46,16 +53,34 @@ const filmsElement = siteMainElement.querySelector('.films');
 // основной список фильмов
 render(filmsElement, createFilmsListTemplate(), 'beforeend');
 const filmsListElement = filmsElement.querySelector('.films-list');
-renderFilmCards(filmsListElement, FILMS_QUANTITY);
-render(filmsListElement, createShowMoreBtnTemplate(), 'beforeend');
+
+if (mockFilms.length > FILMS_PER_STEP) {
+  let renderedFilmsCount = FILMS_PER_STEP;
+
+  render(filmsListElement, createShowMoreBtnTemplate(), 'beforeend');
+
+  const showMoreButton = filmsListElement.querySelector('.films-list__show-more');
+
+  showMoreButton.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    renderFilmCards(filmsListElement, renderedFilmsCount + FILMS_PER_STEP, mockFilms, renderedFilmsCount);
+    renderedFilmsCount += FILMS_PER_STEP;
+
+    if (renderedFilmsCount >= mockFilms.length) {
+      showMoreButton.remove();
+    }
+  });
+}
+
+renderFilmCards(filmsListElement, Math.min(mockFilms.length, FILMS_PER_STEP), mockFilms);
 
 // секция Top rated
 render(filmsElement, createFilmsListExtraTemplate(TOP_RATED_TITLE, TOP_RATED_MODIFIER), 'beforeend');
-renderFilmCards(filmsElement.querySelector(`.films-list--extra.films-list--${TOP_RATED_MODIFIER}`), EXTRA_FILMS_QUANTITY);
+renderFilmCards(filmsElement.querySelector(`.films-list--extra.films-list--${TOP_RATED_MODIFIER}`), EXTRA_MOCK_FILMS_QUANTITY, extraMockFilms);
 
 // секция Most commented
 render(filmsElement, createFilmsListExtraTemplate(MOST_COMMENTED_TITLE, MOST_COMMENTED_MODIFIER), 'beforeend');
-renderFilmCards(filmsElement.querySelector(`.films-list--extra.films-list--${MOST_COMMENTED_MODIFIER}`), EXTRA_FILMS_QUANTITY);
+renderFilmCards(filmsElement.querySelector(`.films-list--extra.films-list--${MOST_COMMENTED_MODIFIER}`), EXTRA_MOCK_FILMS_QUANTITY, extraMockFilms);
 
 // статистика в футере
 const footerStatisticsElement = document.querySelector('.footer__statistics');
@@ -63,4 +88,4 @@ render(footerStatisticsElement, createFooterStatisticsTemplate(), 'beforeend');
 
 // попап
 const bodyElement = document.body;
-render(bodyElement, createPopupTemplate(), 'beforeend');
+render(bodyElement, createPopupTemplate(mockFilms[0]), 'beforeend');
--- src/view/navigation.js
+++ src/view/navigation.js
@@ -1,11 +1,15 @@
+import {getRandomInteger} from '../utils';
+
+const MAX_NAVIGATION_ITEM_VALUE = 20;
+
 export const createNavigationTemplate = () => {
-  return `  <nav class="main-navigation">
+  return `<nav class="main-navigation">
     <div class="main-navigation__items">
-      <a href="#all" class="main-navigation__item main-navigation__item--active">All movies</a>
-      <a href="#watchlist" class="main-navigation__item">Watchlist <span class="main-navigation__item-count">13</span></a>
-      <a href="#history" class="main-navigation__item">History <span class="main-navigation__item-count">4</span></a>
-      <a href="#favorites" class="main-navigation__item">Favorites <span class="main-navigation__item-count">8</span></a>
+      <a href="#all" class="main-navigation__item">All movies</a>
+      <a href="#watchlist" class="main-navigation__item">Watchlist <span class="main-navigation__item-count">${getRandomInteger(1, MAX_NAVIGATION_ITEM_VALUE)}</span></a>
+      <a href="#history" class="main-navigation__item">History <span class="main-navigation__item-count">${getRandomInteger(1, MAX_NAVIGATION_ITEM_VALUE)}</span></a>
+      <a href="#favorites" class="main-navigation__item">Favorites <span class="main-navigation__item-count">${getRandomInteger(1, MAX_NAVIGATION_ITEM_VALUE)}</span></a>
     </div>
-    <a href="#stats" class="main-navigation__additional">Stats</a>
+    <a href="#stats" class="main-navigation__additional main-navigation__additional--active">Stats</a>
   </nav>`;
 };
