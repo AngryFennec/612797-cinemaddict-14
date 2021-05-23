3c3627b1bee0bee0c92dc089f81900c4acb58d41
Angry Fennec <shellfix@gmail.com>
2021-05-23 18:02:42 +0300

    Модель и сортировка

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import Profile from './view/profile';
 import Navigation from './view/navigation';
 import Footer from './view/footer';
 import FilmsPresenter from './presenter/films-presenter';
+import FilmsModel from './model/films';
 
 const MAX_NAVIGATION_ITEM_VALUE = 20;
 const MOCK_FILMS_QUANTITY = 20;
@@ -12,6 +13,8 @@ const MAX_FILMS_QUANTITY = 100000;
 
 // создание моковых массивов
 const mockFilms = new Array(MOCK_FILMS_QUANTITY).fill().map((_, i) => generateFilm(i));
+const filmsModel = new FilmsModel();
+filmsModel.setFilms(mockFilms);
 
 const siteHeaderElement = document.querySelector('.header');
 const siteMainElement = document.querySelector('.main');
@@ -23,8 +26,8 @@ render(siteHeaderElement, new Profile(), RenderPosition.BEFOREEND);
 render(siteMainElement, new Navigation(getRandomInteger(1, MAX_NAVIGATION_ITEM_VALUE), getRandomInteger(1, MAX_NAVIGATION_ITEM_VALUE), getRandomInteger(1, MAX_NAVIGATION_ITEM_VALUE)), RenderPosition.BEFOREEND);
 
 // раздел с фильмами
-const filmsPresenter = new FilmsPresenter(siteMainElement);
-filmsPresenter.init(mockFilms);
+const filmsPresenter = new FilmsPresenter(siteMainElement, filmsModel);
+filmsPresenter.init();
 
 // статистика в футере
 render(document.body, new Footer(getRandomInteger(1000, MAX_FILMS_QUANTITY)), RenderPosition.BEFOREEND);
--- src/presenter/films-presenter.js
+++ src/presenter/films-presenter.js
@@ -11,7 +11,7 @@ import {
   getTopRatedFilms,
   sortByDate,
   sortById,
-  sortByRating
+  sortByRating,
 } from '../utils/presenter';
 import Sort from '../view/sort';
 
@@ -21,12 +21,14 @@ const MOST_COMMENTED_TITLE = 'Most commented';
 const FILMS_PER_STEP = 5;
 
 export default class FilmsPresenter {
-  constructor(container) {
+  constructor(container, filmsModel) {
     this._filmsContainer = container;
     this._films = null;
+    this._filmsModel = filmsModel;
     this._filmsComponent = new Films();
     this._filmsListComponent = null;
     this._sortComponent = new Sort();
+    this._currentSortType = 'default';
     this._mostCommentedFilmsListComponent = new FilmsListExtra(MOST_COMMENTED_TITLE);
     this._topRatedFilmsListComponent = new FilmsListExtra(TOP_RATED_TITLE);
     this._filmsEmptyListComponent = new FilmsEmptyList();
@@ -34,12 +36,22 @@ export default class FilmsPresenter {
     this._sortFilms = this._sortFilms.bind(this);
   }
 
-  init(films) {
+  init() {
     //сортировка
     render(this._filmsContainer, this._sortComponent, RenderPosition.BEFOREEND);
     this._sortComponent.setClickHandler(this._sortFilms);
-    this._films = films.slice(); // копируем в презентер массив фильмов
     this._initFilms();
+  }
+
+  _getFilms() {
+    switch (this._currentSortType) {
+      case 'date':
+        return this._filmsModel.getFilms().slice().sort(sortByDate);
+      case 'rating':
+        return this._filmsModel.getFilms().slice().sort(sortByRating);
+      default:
+        return this._filmsModel.getFilms().slice().sort(sortById);
+    }
   }
 
   _initFilms() {
@@ -53,10 +65,10 @@ export default class FilmsPresenter {
     render(this._filmsComponent.getElement(), this._mostCommentedFilmsListComponent, RenderPosition.BEFOREEND);
 
     // секция Top rated
-    this._renderFilmCards(this._topRatedFilmsListComponent.getContainer(), getTopRatedFilms(this._films.slice()));
+    this._renderFilmCards(this._topRatedFilmsListComponent.getContainer(), getTopRatedFilms(this._getFilms()));
 
     // секция Most commented
-    this._renderFilmCards(this._mostCommentedFilmsListComponent.getContainer(), getMostCommentedFilms(this._films.slice()));
+    this._renderFilmCards(this._mostCommentedFilmsListComponent.getContainer(), getMostCommentedFilms(this._getFilms()));
   }
 
   _renderFilmCards(container, films) {
@@ -81,7 +93,7 @@ export default class FilmsPresenter {
 
   _renderFilms() {
     const filmsElement = this._filmsComponent.getElement();
-    if (!this._films || this._films.length === 0) {
+    if (!this._getFilms() || this._getFilms().length === 0) {
       render(filmsElement, this._filmsEmptyListComponent, RenderPosition.BEFOREEND);
       return;
     }
@@ -95,28 +107,29 @@ export default class FilmsPresenter {
     this._filmsListComponent = newFilmsListInstance;
 
 
-    const iterator = getNextRenderCardIterator(this._films, FILMS_PER_STEP);
+    const iterator = getNextRenderCardIterator(this._getFilms(), FILMS_PER_STEP);
     const {value: filmsPart} = iterator.next();
     this._renderFilmCards(this._filmsListComponent.getContainer(), filmsPart);
 
-    if (this._films.length > FILMS_PER_STEP) {
+    if (this._getFilms().length > FILMS_PER_STEP) {
       this._renderShowMoreButton(iterator);
     }
   }
 
   // эта функция оставлена в классе, потому что она передается как коллбэк и в ней дергается метод класса
   _sortFilms(sortType) {
-    switch (sortType) {
-      case 'default':
-        this._films.sort(sortById);
-        break;
-      case 'date':
-        this._films.sort(sortByDate);
-        break;
-      case 'rating':
-        this._films.sort(sortByRating);
-        break;
-    }
+    this._currentSortType = sortType;
+    // switch (sortType) {
+    //   case 'default':
+    //     this._films.sort(sortById);
+    //     break;
+    //   case 'date':
+    //     this._films.sort(sortByDate);
+    //     break;
+    //   case 'rating':
+    //     this._films.sort(sortByRating);
+    //     break;
+    // }
     this._renderFilms();
   }
 }
