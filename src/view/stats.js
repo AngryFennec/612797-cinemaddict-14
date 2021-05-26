d194f61b7d28b7c224bc039faa89d6ac98998226
Angry Fennec <shellfix@gmail.com>
2021-05-26 08:31:42 +0300

    часть статистики

--- src/view/stats.js
+++ src/view/stats.js
@@ -1,4 +1,7 @@
 import AbstractView from './abstract-view';
+import { Chart } from 'chart.js';
+import ChartDataLabels from 'chartjs-plugin-datalabels';
+
 
 export const createStatsTemplate = () => {
   return `<section class="statistic">
@@ -56,7 +59,81 @@ export const createStatsTemplate = () => {
 };
 
 export default class Stats extends AbstractView {
+
+  constructor() {
+    super();
+    this._init();
+  }
+
+  _init() {
+    this.createCharts();
+  }
+
   getTemplate() {
     return createStatsTemplate();
+  }
+
+  createCharts() {
+    const BAR_HEIGHT = 50;
+    const statisticCtx = this.getElement().querySelector('.statistic__chart');
+
+    statisticCtx.height = BAR_HEIGHT * 5;
+
+    return new Chart(statisticCtx, {
+      plugins: [ChartDataLabels],
+      type: 'horizontalBar',
+      data: {
+        labels: ['Sci-Fi', 'Animation', 'Fantasy', 'Comedy', 'TV Series'],
+        datasets: [{
+          data: [11, 8, 7, 4, 3],
+          backgroundColor: '#ffe800',
+          hoverBackgroundColor: '#ffe800',
+          anchor: 'start',
+        }],
+      },
+      options: {
+        plugins: {
+          datalabels: {
+            font: {
+              size: 20,
+            },
+            color: '#ffffff',
+            anchor: 'start',
+            align: 'start',
+            offset: 40,
+          },
+        },
+        scales: {
+          yAxes: [{
+            ticks: {
+              fontColor: '#ffffff',
+              padding: 100,
+              fontSize: 20,
+            },
+            gridLines: {
+              display: false,
+              drawBorder: false,
+            },
+            barThickness: 24,
+          }],
+          xAxes: [{
+            ticks: {
+              display: false,
+              beginAtZero: true,
+            },
+            gridLines: {
+              display: false,
+              drawBorder: false,
+            },
+          }],
+        },
+        legend: {
+          display: false,
+        },
+        tooltips: {
+          enabled: false,
+        },
+      },
+    });
   }
 }
